6b5e0070d97c64e4aec4cd5062be592b2f7a3d2d
Carver Harrison <pcarverb@hotmail.com>
2016-08-03 18:49:48 -0700

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,13 @@
-<?php
-  include "Parsedown.php";
-  $Parsedown = new Parsedown();
-  echo $Parsedown->text(file_get_contents("doc.md"));
-?>
+<html>
+  <head>
+    <title>ShiftLua Documentation</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  </head>
+  <body>
+    <?php
+      include "Parsedown.php";
+      $Parsedown = new Parsedown();
+      echo $Parsedown->text(file_get_contents("doc.md"));
+    ?>
+  </body>
+</html>
